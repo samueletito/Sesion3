463531ba2c8d124d0d5229a3d8d87ea9e08f7bc4
samueletito <ssandia1104@gmail.com>
2024-10-11 10:29:45 +0200

    Primer commit: generar pruebas (testIngresar)

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	static Cuenta cuenta;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta = new Cuenta(0);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar(1);
+		
+		assertEquals(1, cuenta.getSaldo());
+	}
+
+}
