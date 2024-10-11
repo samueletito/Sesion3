f3e6f72e40681fe8ba2458c6da0ddab466214158
samueletito <ssandia1104@gmail.com>
2024-10-11 11:15:44 +0200

    Sexto commit: generar las pruebas

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,7 +14,7 @@ import pkg.Cuenta;
 
 class CuentaTest {
 
-	Cuenta cuenta;
+	Cuenta cuenta, cuenta12345, cuenta67890;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -24,7 +26,9 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		cuenta = new Cuenta(0d);
+		cuenta = new Cuenta(0d, "00000", "Samuel", new ArrayList<Movimiento>());
+		cuenta12345 = new Cuenta(50d, "12345", "Ana", new ArrayList<Movimiento>());
+		cuenta67890 = new Cuenta(0d, "67890", "Adrian", new ArrayList<Movimiento>());
 	}
 
 	@AfterEach
@@ -43,6 +47,26 @@ class CuentaTest {
 		cuenta.retirar(1d);
 		
 		assertEquals(-1, cuenta.getSaldo());
+	}
+	
+	@Test
+	void test0014() {
+		cuenta12345.retirar(200d);
+		cuenta67890.retirar(350d);
+		cuenta12345.ingresar(100d);
+		cuenta67890.retirar(200d);
+		cuenta67890.retirar(150d);
+		cuenta12345.retirar(200d);
+		cuenta67890.retirar(50d);
+		cuenta67890.retirar(100d);
+		
+		boolean comprobar = false;
+		
+		if (cuenta12345.getSaldo() == -250d && cuenta67890.getSaldo() == -450d) {
+			comprobar = true;
+		}
+		
+		assertTrue(comprobar);
 	}
 
 }
